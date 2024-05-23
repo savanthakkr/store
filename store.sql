8e0bcfeb1a8e7042e052e65fbc139c4f022eed25
savan ponda <sponda@netclues.lan>
2024-05-23 11:58:57 +0530

    final

--- store.sql
+++ store.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 08, 2024 at 06:09 AM
--- Server version: 5.7.14
--- PHP Version: 8.2.13
+-- Generation Time: May 23, 2024 at 06:28 AM
+-- Server version: 8.2.0
+-- PHP Version: 8.1.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,17 +24,39 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `password`, `createdAt`) VALUES
+(1, 'savanponda11@gmail.com', 'savanponda', '2024-05-23 04:55:33');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `category`
 --
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `categoryName` varchar(100) DEFAULT NULL,
-  `createdBy` int(11) DEFAULT NULL,
+  `createdBy` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `createdBy` (`createdBy`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `category`
@@ -42,7 +64,8 @@ CREATE TABLE IF NOT EXISTS `category` (
 
 INSERT INTO `category` (`id`, `categoryName`, `createdBy`) VALUES
 (1, 'test', 1),
-(4, 'cloth', 1);
+(4, 'cloth', 1),
+(5, 'football', 5);
 
 -- --------------------------------------------------------
 
@@ -52,13 +75,14 @@ INSERT INTO `category` (`id`, `categoryName`, `createdBy`) VALUES
 
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   `description` text,
-  `categoryId` int(11) DEFAULT NULL,
+  `categoryId` int DEFAULT NULL,
+  `categoryName` varchar(150) NOT NULL,
   `price` decimal(10,2) DEFAULT NULL,
   `images` text,
-  `createdBy` int(11) DEFAULT NULL,
+  `createdBy` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `categoryId` (`categoryId`),
   KEY `createdBy` (`createdBy`)
@@ -68,12 +92,47 @@ CREATE TABLE IF NOT EXISTS `product` (
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `description`, `categoryId`, `price`, `images`, `createdBy`) VALUES
-(3, 'shirt', 'good shirt', 1, 99.55, '', NULL),
-(4, 't-shirt', 'casual shirt', 1, 109.00, '', NULL),
-(5, 'pant', 'formal pant', 1, 109.00, '', NULL),
-(7, 'savan', '  savan product', 1, 1000.00, '', NULL),
-(8, 'savan', '  savan product', 1, 1000.00, '', NULL);
+INSERT INTO `product` (`id`, `name`, `description`, `categoryId`, `categoryName`, `price`, `images`, `createdBy`) VALUES
+(3, 'shirt', 'good shirt', 1, '', 99.55, '', NULL),
+(4, 't-shirt', 'casual shirt', 1, '', 109.00, '', NULL),
+(5, 'pant', 'formal pant', 1, '', 109.00, '', NULL),
+(7, 'savan', '  savan product', 1, '', 1000.00, '', NULL),
+(8, 'savan', '  savan product', 1, '', 1000.00, '', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productsimage`
+--
+
+DROP TABLE IF EXISTS `productsimage`;
+CREATE TABLE IF NOT EXISTS `productsimage` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `images` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `hobbies` text NOT NULL,
+  `profile_pic` blob NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -83,7 +142,7 @@ INSERT INTO `product` (`id`, `name`, `description`, `categoryId`, `price`, `imag
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `firstName` varchar(50) DEFAULT NULL,
   `lastName` varchar(50) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -94,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `profile_pic` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -103,7 +162,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `password`, `gender`, `hobbies`, `userRole`, `profile_pic`) VALUES
 (1, 'dhruval', 'hingu', 'dhruval@mail.com', '$2b$10$.n2J8iWsBzcuWubJj3zWN.E2757KxUfS8Alg0036WfJIpMkkh/gAy', 'Male', 'read, write', 'Admin', ''),
 (2, 'yagnesh', 'patel', 'yagnesh@mail.com', '$2b$10$N5kRMzEJZvgcdL9Pc0eOqut80hfgGETLfhCOmIz6G5r8f6H/TZ3vO', 'Male', 'read, write', 'User', ''),
-(3, 'mail', 'mail', 'mail@mail.com', '$2b$10$36SueSJ2sq8kn2zNm3fFlOMy54o.JpDTzSmVyRkejqBYMP2iJAg4u', 'Male', 'read, write', 'Admin', '');
+(3, 'mail', 'mail', 'mail@mail.com', '$2b$10$36SueSJ2sq8kn2zNm3fFlOMy54o.JpDTzSmVyRkejqBYMP2iJAg4u', 'Male', 'read, write', 'Admin', ''),
+(5, 'savan', 'ponda', 'savanponda11@gmail.com', '$2b$10$Ekg1wB3IdFatkz/tnqhmeezScja4NbKLqk99CqH6m59QKDAgSc48u', 'Male', 'cricket,football', NULL, '/public/assets/1716435535362.md');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
